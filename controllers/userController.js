7abca48af3078171d6a74a0082f6c7b80a28c8e2
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2018-05-29 19:46:18 +0200

    Merge

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,72 +1,84 @@
-
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const promisify = require('es6-promisify');
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const promisify = require("es6-promisify");
 
 exports.loginForm = (req, res) => {
-    const isMobile = req.device.type === 'phone' || req.device.type === 'tablet';
+  const isMobile = req.device.type === "phone" || req.device.type === "tablet";
 
-    res.render('login', {title: 'Login', isMobile});
-}
+  res.render("login", { title: "Login", isMobile });
+};
 
 exports.registerForm = (req, res) => {
-    const isMobile = req.device.type === 'phone' || req.device.type === 'tablet';
+  const isMobile = req.device.type === "phone" || req.device.type === "tablet";
 
-    res.render('register', {title: 'Register', isMobile});
-}
+  res.render("register", { title: "Register", isMobile });
+};
 
-exports.validateRegister = (req, res, next) => {
-    req.sanitizeBody('name');
-    req.checkBody('name', 'You must supply a name!').notEmpty();
-    req.checkBody('email', 'That Email is not valid').isEmail();
-    req.sanitizeBody('email').normalizeEmail({
-        remove_dots: false,
-        gmail_remove_dots: false,
-        remove_extension: false,
-        gmail_remove_subaddress: false
+exports.validateRegister = async (req, res, next) => {
+  req.sanitizeBody("name");
+  req.checkBody("name", "You must supply a name!").notEmpty();
+  req.checkBody("email", "That Email is not valid").isEmail();
+  req.sanitizeBody("email").normalizeEmail({
+    remove_dots: false,
+    gmail_remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  });
+  req.checkBody("password", "Password cannot be empty!").notEmpty();
+  req
+    .checkBody("password-confirm", "Confirm Password cannot be empty!")
+    .notEmpty();
+  req
+    .checkBody("password-confirm", "Your passwords do not match!")
+    .equals(req.body.password);
+
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    req.flash("error", `${req.body.email} is already registered`);
+    return res.redirect("back");
+  }
+  const errors = req.validationErrors();
+  if (errors && user) {
+    req.flash("error", errors.map(err => err.msg));
+    res.render("register", {
+      title: "Register",
+      body: req.body,
+      flashes: req.flash()
     });
-    req.checkBody('password', 'Password cannot be empty!').notEmpty();
-    req.checkBody('password-confirm', 'Confirm Password cannot be empty!').notEmpty();
-    req.checkBody('password-confirm', 'Your passwords do not match!').equals(req.body.password);
+    return;
+  }
 
-    const errors = req.validationErrors();
-    if(errors) {
-       req.flash('error', errors.map(err => err.msg));
-       res.render('register', {title: 'Register', body: req.body, flashes: req.flash()})
-       return;
-    }
-    
-    next();
+  next();
 };
 
 exports.register = async (req, res, next) => {
-    const user = new User({
-        email: req.body.email, 
-        name: req.body.name,
-        admin: false
-    });
-    const register = promisify(User.register, User);
-    await register(user, req.body.password);
-    next();
+  const user = new User({
+    email: req.body.email,
+    name: req.body.name,
+    admin: false
+  });
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
+  next();
 };
 
 exports.account = (req, res) => {
-    const isMobile = req.device.type === 'phone' || req.device.type === 'tablet';
+  const isMobile = req.device.type === "phone" || req.device.type === "tablet";
 
-    res.render('account', {title: 'Login', isMobile })
-}
+  res.render("account", { title: "Login", isMobile });
+};
 
 exports.updateAccount = async (req, res) => {
-    const updates = {
-        name: req.body.name,
-        email: req.body.email
-    };
+  const updates = {
+    name: req.body.name,
+    email: req.body.email
+  };
 
-    const user = await User.findOneAndUpdate(
-        { _id: req.user._id },
-        { $set: updates },
-        { new: true, runValidators: true, context: 'query' }
-    );
-    req.flash('success', 'Your account has been updated!');
-    res.redirect('back');
-}
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: "query" }
+  );
+  req.flash("success", "Your account has been updated!");
+  res.redirect("back");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,126 +1,120 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const questController = require('../controllers/questController');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
-const { catchErrors } = require('../handlers/errorHandlers');
+const questController = require("../controllers/questController");
+const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
+router.get("/", catchErrors(questController.getQuests));
+router.get("/quests", catchErrors(questController.getQuests));
 
-router.get('/', 
-  catchErrors(questController.getQuests));
-router.get('/quests',
-  catchErrors(questController.getQuests));
+router.get("/quests/page/:page", catchErrors(questController.getQuests));
 
-router.get('/quests/page/:page',
-  catchErrors(questController.getQuests));
-
-router.get('/quests/:id/edit',
-  catchErrors(questController.editQuest));
-router.get('/add',
+router.get("/quests/:id/edit", catchErrors(questController.editQuest));
+router.get(
+  "/add",
   authController.isLoggedIn,
   authController.isCreativeOrAdmin,
-  questController.addQuest); 
-router.post('/add',
-  questController.upload, 
-  catchErrors(questController.resizeImage('quest')),
-  catchErrors(questController.resizeImageMedium('quest')),
-  catchErrors(questController.resizeImageLarge('quest')), 
+  questController.addQuest
+);
+router.post(
+  "/add",
+  questController.upload,
+  catchErrors(questController.resizeImage("quest")),
+  catchErrors(questController.resizeImageMedium("quest")),
+  catchErrors(questController.resizeImageLarge("quest")),
   catchErrors(questController.createQuest)
 );
 
-router.get('/search',
-  catchErrors(questController.searchCity)
-);
-router.get('/city/:city', 
-  catchErrors(questController.citySearch));
+router.get("/search", catchErrors(questController.searchCity));
+router.get("/city/:city", catchErrors(questController.citySearch));
 
-router.get('/city/:city/page/:page',
-  catchErrors(questController.citySearch));
+router.get("/city/:city/page/:page", catchErrors(questController.citySearch));
 
-router.get('/city/:city/tags/:tag',
-  catchErrors(questController.citySearch));
+router.get("/city/:city/tags/:tag", catchErrors(questController.citySearch));
 
-router.post('/add/:id',
-  questController.upload, 
-  catchErrors(questController.resizeImage('quest')),
-  catchErrors(questController.resizeImageMedium('quest')),
-  catchErrors(questController.resizeImageLarge('quest')), 
-  catchErrors(questController.updateQuest));
-
-router.get('/quest/:slug', 
-  catchErrors(questController.getQuestBySlug));
-
-router.post('/completedQuest/:id',
+router.post(
+  "/add/:id",
   questController.upload,
-  catchErrors(questController.resizeImage('completed')),
-  catchErrors(questController.resizeImageMedium('completed')),
-  catchErrors(questController.resizeImageLarge('completed')), 
+  catchErrors(questController.resizeImage("quest")),
+  catchErrors(questController.resizeImageMedium("quest")),
+  catchErrors(questController.resizeImageLarge("quest")),
+  catchErrors(questController.updateQuest)
+);
+
+router.get("/quest/:slug", catchErrors(questController.getQuestBySlug));
+
+router.post(
+  "/completedQuest/:id",
+  questController.upload,
+  catchErrors(questController.resizeImage("completed")),
+  catchErrors(questController.resizeImageMedium("completed")),
+  catchErrors(questController.resizeImageLarge("completed")),
   catchErrors(questController.removeCompletedBookmark),
   catchErrors(questController.incrementCompletedAmount),
-  catchErrors(questController.completeQuest));
+  catchErrors(questController.completeQuest)
+);
 
-router.get('/tags', 
-  catchErrors(questController.getQuestByTag));
+router.get("/tags", catchErrors(questController.getQuestByTag));
 
-router.get('/tags/:tag', 
-  catchErrors(questController.getQuestByTag));
-  
-router.get('/tags/:tag/:page', 
-catchErrors(questController.getQuestByTag));
+router.get("/tags/:tag", catchErrors(questController.getQuestByTag));
+
+router.get("/tags/:tag/:page", catchErrors(questController.getQuestByTag));
 
 //Login routes
-router.get('/login', userController.loginForm); 
-router.post('/login', authController.login); 
-  
-router.get('/register', userController.registerForm);
-router.post('/register', 
-  userController.validateRegister,
+router.get("/login", userController.loginForm);
+router.post("/login", authController.login);
+
+router.get("/register", userController.registerForm);
+router.post(
+  "/register",
+  catchErrors(userController.validateRegister),
   userController.register,
-  authController.login);
+  authController.login
+);
 
-router.get('/logout', 
-  authController.logout);
+router.get("/logout", authController.logout);
 
-router.get('/account', 
-  authController.isLoggedIn,
-  userController.account);
+router.get("/account", authController.isLoggedIn, userController.account);
 
-router.post('/account', 
-  catchErrors(userController.updateAccount));
-  
-router.post('/account/forgot',
-catchErrors(authController.forgot));
+router.post("/account", catchErrors(userController.updateAccount));
 
-router.get('/account/reset/:token',
-  catchErrors(authController.reset));
+router.post("/account/forgot", catchErrors(authController.forgot));
 
-router.post('/account/reset/:token',
+router.get("/account/reset/:token", catchErrors(authController.reset));
+
+router.post(
+  "/account/reset/:token",
   authController.confirmedPassords,
-  catchErrors(authController.update));
+  catchErrors(authController.update)
+);
 
-router.get('/bookmarked', 
+router.get(
+  "/bookmarked",
   authController.isLoggedIn,
-  catchErrors(questController.getBookmarks));
+  catchErrors(questController.getBookmarks)
+);
 
-router.get('/bookmarked/:page',
+router.get(
+  "/bookmarked/:page",
   authController.isLoggedIn,
-  catchErrors(questController.getBookmarks));
+  catchErrors(questController.getBookmarks)
+);
 
-router.get('/completed', 
+router.get(
+  "/completed",
   authController.isLoggedIn,
-  catchErrors(questController.getCompletedQuests));
+  catchErrors(questController.getCompletedQuests)
+);
 
 //Api end points
-router.get('/api/search', 
-catchErrors(questController.searchQuests));
+router.get("/api/search", catchErrors(questController.searchQuests));
 
-router.post('/api/quests/:id/heart',
-  catchErrors(questController.heartQuest));
+router.post("/api/quests/:id/heart", catchErrors(questController.heartQuest));
 
-router.post('/api/quests/:id/completed',
-  catchErrors(questController.completeQuest));
-  
-
-
+router.post(
+  "/api/quests/:id/completed",
+  catchErrors(questController.completeQuest)
+);
 
 module.exports = router;
